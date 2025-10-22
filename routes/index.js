cbde4ddba1bba00b3ecc8c962d6c5f46f5df705f
mersubi <emirseitveliev555@gmail.com>
2025-10-22 08:51:52 +0300

    .

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,15 @@ router.get('/', function (req, res, next) {
 });
 /* Страница 86*/
 router.get('/86_bus', function (req, res, next) {
-  res.send("<h1>86 Чистенькое - Загородный</h1>")
+  res.send("<h1>83/86 (485-23) мкрн. Загородный - с. Чистенькое (с.Трёхпрудное)</h1>")
 });
-
+/* Страница 57*/
+router.get('/57_bus', function (req, res, next) {
+  res.send("<h1>57 (495-23) АШАН (ТЦ 'Южная Галерея') - с. Левадки</h1>")
+});
+/* Страница 91*/
+router.get('/91_bus', function (req, res, next) {
+  res.send("<h1>91 (558-23) ГРЭС - Ак Мечеть (п. Ешиль-Даг)</h1>")
+});
 
 module.exports = router;
