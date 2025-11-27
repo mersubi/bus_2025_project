e2f99147743384bc18aa5783c73a86f2f44ce696
mersubi <emirseitveliev555@gmail.com>
2025-11-27 12:54:33 +0300

    bus_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
 
-const Bus = mongoose.model('Bus', { name: String });
+var schema = mongoose.Schema({ name: String })
 
 
-const busik = new Bus({ name: 'Capa' });
-busik.save().then(() => console.log('brbrbrbr'));
+
+
+schema.methods.motor = function () {
+    console.log(this.name + " запустил мотор")
+}
+
+
+
+
+const Bus = mongoose.model('Bus', schema);
+
+
+
+
+
+
+const busik = new Bus({ name: 'Vector Next' });
+busik.save().then(() => busik.motor());
