f0a2f0b30d71b5396306f7a0ee392a32ce0db4e8
mersubi <emirseitveliev555@gmail.com>
2025-12-09 12:11:52 +0300

    bus_8_3

--- routes/buses.js
+++ routes/buses.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var Bus = require('../models/bus').Bus;
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('Новый маршрутизатор, для маршрутов, начинающихся с buses');
 });
-/* Страница автобусов */
-router.get("/:nick", function (req, res, next) {
-    res.send(req.params.nick);
+/* Страница котов */
+router.get("/:nick", async function (req, res, next) {
+    var buses = await Bus.find({ nick: req.params.nick });
+    console.log(buses)
+    if (!buses.length) return next(new Error("Нет такого автобуса, выпущенного Mercedes-Benz"))
+    var bus = buses[0];
+    res.render('bus', {
+        title: bus.title,
+        picture: bus.avatar,
+        desc: bus.desc
+    })
 });
+
 
 module.exports = router;
