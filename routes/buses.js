00ab3caf14562329e3c6f43d503220ce59a95cd6
mersubi <emirseitveliev555@gmail.com>
2025-12-09 12:54:42 +0300

    bus_8_4

--- routes/buses.js
+++ routes/buses.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Bus = require('../models/bus').Bus;
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('Новый маршрутизатор, для маршрутов, начинающихся с buses');
-});
+
 /* Страница котов */
 router.get("/:nick", async function (req, res, next) {
     var buses = await Bus.find({ nick: req.params.nick });
