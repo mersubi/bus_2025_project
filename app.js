01eb392996c40a1114c5ef83c0161388b20bfffe
mersubi <emirseitveliev555@gmail.com>
2025-12-09 11:59:27 +0300

    bus_8_2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/bus2025')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexBuses = require('./routes/buses');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/buses', indexBuses);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
