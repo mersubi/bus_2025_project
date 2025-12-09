b5e4b680e6514c462ad0e6353cfe7e3574df1d40
mersubi <emirseitveliev555@gmail.com>
2025-12-09 17:25:01 +0300

    bus_9_4

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.use(session({
   })
 }));
 
+
+app.use(function (req, res, next) {
+  req.session.counter = req.session.counter + 1 || 1
+  next()
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/buses', indexBuses);
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  req.session.greeting = "Hi!!!";
-  res.render('index', { title: 'Mercedes-Benz Buses' });
+  res.render('index', { title: 'Mercedes-Benz Buses', counter: req.session.counter });
 });
+
 
 
 
