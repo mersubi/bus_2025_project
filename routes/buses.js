26c9d9368869581281272789ca989a4eca163f29
mersubi <emirseitveliev555@gmail.com>
2025-12-10 08:19:03 +0300

    bus_10_8

--- routes/buses.js
+++ routes/buses.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Bus = require('../models/bus').Bus;
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* Страница котов */
-router.get("/:nick", async function (req, res, next) {
+router.get("/:nick", checkAuth, async function (req, res, next) {
     var buses = await Bus.find({ nick: req.params.nick });
     console.log(buses)
     if (!buses.length) return next(new Error("Нет такого автобуса, выпущенного Mercedes-Benz"))
